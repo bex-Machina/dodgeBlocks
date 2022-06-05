68cda115c07728b624f88c697d47122e42379c7f
bex-Machina <bex-Machina@users.noreply.github.com>
2022-06-05 15:55:19 +0100

    Scale and rotation
    Added random scale and rotation to the game obstacles

--- dodgeBlocksUnity/Assets/Spawmer.cs
+++ dodgeBlocksUnity/Assets/Spawmer.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Spawmer : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject cube;
+    public GameObject Block;
     Vector2 halfWidth;
     public float secondsBetweenSpawns = 1;
     public float nextSpawnTime;
+    public Vector2 spawnSizeMinMax;
+    public float spawnAngleMax;
     void Start()
     {
         halfWidth = new Vector2( Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize); 
@@ -19,9 +21,12 @@ public class Spawmer : MonoBehaviour
     {
         if (Time.time > nextSpawnTime)
         {
+            float spawnAngle = Random.Range(-spawnAngleMax, spawnAngleMax);
+            float spawnSize = Random.Range(spawnSizeMinMax.x, spawnSizeMinMax.y);
             nextSpawnTime = Time.time + secondsBetweenSpawns;
-            Vector2 spawnPoint = new Vector2 (Random.Range(-halfWidth.x, halfWidth.x), halfWidth.y+0.5f);
-            Instantiate(cube, spawnPoint, Quaternion.identity);
+            Vector2 spawnPoint = new Vector2 (Random.Range(-halfWidth.x, halfWidth.x), halfWidth.y+ spawnSize);
+            GameObject newBlock = (GameObject)Instantiate(Block, spawnPoint, Quaternion.Euler(Vector3.forward*spawnAngle));
+            newBlock.transform.localScale = Vector2.one * spawnSize;
         }
     }
 }
